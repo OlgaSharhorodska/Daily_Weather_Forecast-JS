968fc69dae86f2e34ba0ff56ea137e60f6ec7f03
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-08-22 22:01:04 +0300

    update js

--- main.js
+++ main.js
@@ -58,6 +58,13 @@ function serviceWeather(city, days) {
 
 }
 
-
+function createMarkup(arr){
+return arr.map(({date, day : {avgtemp_c , condition : {icon, text}}}) => `<li class="weather-card">
+    <img src="${icon}" alt="${text}" class="weather-icon"/>
+    <h2 class="date">${date}</h2>
+    <h3 class="weather-text">${text}</h3>
+    <h3 class="temperature">${avgtemp_c} °C</h3>
+</li>`).join('')
+}
 
 
